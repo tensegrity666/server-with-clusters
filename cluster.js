ab37de6c8c3d7a6ada91a81766564901cf60d631
tensegrity666 <tensegrity666@icloud.com>
2020-11-28 20:07:34 +0300

    fix: delete node_modules from remote

--- cluster.js
+++ cluster.js
@@ -1,16 +1,24 @@
 const cluster = require('cluster');
 const os = require('os');
 
+const DEFAULT_CPU_COUNT_FOR_MOBILES = 4;
+
 if (cluster.isMaster) {
-	const cpusCount = os.cpus().length || 8;
+	const cpusCount = os.cpus().length || DEFAULT_CPU_COUNT_FOR_MOBILES;
 	console.log(os.platform());
 	console.log(os.freemem());
 	console.log(`CPUs: ${cpusCount}`)
 
 	for (let i = 0; i < cpusCount - 1; i++) {
-		const fork = cluster.fork();
-		fork.on('exit', () => cluster.fork());
-	};
+		cluster.fork();	
+	}
+
+	cluster.on('exit', (worker, code) => {
+		console.log(`Worker ${worker.process.pid} was killed`);
+		if (code !== 0) {
+			cluster.fork();
+		}
+	});
 };
 
 if (cluster.isWorker) {
--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const pid = process.pid
 
-http
+const server = http
 	.createServer((req, res) => {
 		for(let i = 0; i < 1e7; i++) {}
 		res.end('response')
@@ -9,3 +9,15 @@ http
 	.listen(3000, () => {
 		console.log(`server started, pid: ${pid}`)
 	});
+
+process.on('SIGTERM', () => {
+	server.close(() => process.exit(0));
+});
+
+process.on('SIGINT', () => {
+	server.close(() => process.exit(0));
+});
+
+process.on('SIGUSR2', () => {
+	server.close(() => process.exit(1));
+});
